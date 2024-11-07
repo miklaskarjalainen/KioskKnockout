292c562f0220b2a3c092c3bd41b9f166a167baa8
miklas karjalainen <miklas.karjalainen@gmail.com>
2024-11-07 17:41:32 +0200

    Player can now be controlled with different input_actions.

--- src/actors/player.gd
+++ src/actors/player.gd
@@ -1,5 +1,11 @@
 extends CharacterBody3D
 
+@export var InputLeft := "kb_left"
+@export var InputRight := "kb_right"
+@export var InputDown := "kb_down"
+@export var InputUp := "kb_up"
+@export var InputJump := "kb_jump"
+
 @export var Speed: float = 5.0
 @export var JumpVelocity: float = 4.5
 
@@ -9,12 +15,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("kb_up") and is_on_floor():
+	if Input.is_action_just_pressed(InputJump) and is_on_floor():
 		velocity.y = JumpVelocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_axis("kb_left", "kb_right")
+	var input_dir := Input.get_axis(InputLeft, InputRight)
 	var direction := (transform.basis * Vector3(input_dir, 0, 0)).normalized()
 	if direction:
 		velocity.x = direction.x * Speed
