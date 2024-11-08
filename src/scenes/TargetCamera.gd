b31c475fc048d2c27b05b4bf2d997d78e91b1855
Zefyro <71092443+Zefyro@users.noreply.github.com>
2024-11-08 17:56:56 +0200

    Turn the camera into something with more broad use cases
    Allows setting an Offset for the camera X/Y from the editor and supports different CameraModes for scenes

--- src/scenes/TargetCamera.gd
+++ src/scenes/TargetCamera.gd
@@ -1,11 +1,14 @@
 extends Camera3D
 class_name TargetCamera
 
+const MAGIC_NUMBER: float = 0.035
+
 @export var Targets: Array[Node3D] = []
 @export var MaxZoom: float = 25.0
 @export_range(0.1, 16.0) var Smoothing: float = 2.4
+@export_enum("Set Y", "Follow Y") var CameraMode: String = "Set Y"
+@export var Offset: Vector2 = Vector2(0, 2.33)
 
-@onready var _center_offset := Vector2(global_position.x, global_position.y)
 var _initial_distance: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
@@ -18,9 +21,6 @@ func _vec3_to_vec2(vec: Vector3) -> Vector2:
 	return Vector2(vec.x, vec.y)
 
 func _calculate_center() -> Vector2:
-	if not Targets:
-		return Vector2(0.0, global_position.y)
-	
 	var point: Vector2
 	for target in Targets:
 		point += _vec3_to_vec2(target.global_position)
@@ -29,20 +29,29 @@ func _calculate_center() -> Vector2:
 	return point
 
 func _max_distance_between_targets() -> float:
-	assert(Targets.size() == 2, "Only works if there are 2 targets")
-	var tx0 := Targets[0].global_position.x
-	var tx1 := Targets[1].global_position.x
-	return abs(tx0 - tx1)
+	var least_x: float
+	var most_x: float
+	for target in Targets:
+		if target.global_position.x < least_x:
+			least_x = target.global_position.x
+		if target.global_position.x > most_x:
+			most_x = target.global_position.x
+	
+	return abs(least_x - most_x)
 
 func _physics_process(delta: float) -> void:
 	# X & Y
 	var target_camera_pos := Vector3()
-	target_camera_pos.x = _calculate_center().x
-	target_camera_pos.y = global_position.y
+	var center: Vector2 = _calculate_center() + Offset
+	target_camera_pos.x = center.x
+	match CameraMode:
+		"Set Y":
+			target_camera_pos.y = global_position.y
+		"Follow Y":
+			target_camera_pos.y = center.y
 	
 	# Z
 	var distance := _max_distance_between_targets()
-	var MAGIC_NUMBER := 0.035
 	target_camera_pos.z = _initial_distance + (distance * distance * MAGIC_NUMBER)
 	target_camera_pos.z = clamp(target_camera_pos.z, 0.0, MaxZoom)
 	
