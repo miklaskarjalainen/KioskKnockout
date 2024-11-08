c0cce75d527d4c93b59add1496c3aeb63c721dab
Zefyro <71092443+Zefyro@users.noreply.github.com>
2024-11-08 11:19:26 +0200

    fixes, camera scrolling

--- src/scenes/TargetCamera.gd
+++ src/scenes/TargetCamera.gd
@@ -2,16 +2,14 @@ extends Camera3D
 class_name TargetCamera
 
 @export var Targets: Array[Node3D] = []
-@export var MinZoom := 25.0
-@export var MaxZoom := 50.0
-@export_range(0.1, 1.0) var MoveSpeed := 0.5
-@export_range(0.1, 0.4) var Smoothing := 0.3
-@export var Margin: Vector2 = Vector2(400, 200)
+@export var MaxZoom: float = 40.0
+@export_range(0.1, 1.0) var MaxMoveSpeed: float = 0.5
+@export_range(0.1, 0.4) var Smoothing: float = 0.3
 
 
 @onready var _center_offset := Vector2(global_position.x, global_position.y)
-var _initial_distance := 0.0
-var _initial_proportion := 0.0
+var _initial_distance: float = 0.0
+var _initial_proportion: float = 0.0
 var _viewport_rect := Rect2()
 
 # Called when the node enters the scene tree for the first time.
@@ -27,12 +25,23 @@ func _ready() -> void:
 var _move_tween := Tween.new()
 
 func _process(delta: float) -> void:
-	#_multi_target()
-	var target_z := _calculate_position(_calculate_targets_rect(), _calculate_unprojected_rect())
-	global_position.z = lerp(global_position.z, target_z, delta * 16)
+	var target_camera_position: Vector3 = _calculate_position(_calculate_unprojected_rect())
+	Console.info("%s" % target_camera_position)
+	global_position = lerp(global_position, target_camera_position, delta * 16)
 
 func _vec3_to_vec2(vec: Vector3) -> Vector2:
 	return Vector2(vec.x, vec.y)
+
+func _calculate_center() -> Vector2:
+	if not Targets:
+		return Vector2(0.0, global_position.y)
+	
+	var point: Vector2
+	for target in Targets:
+		point += _vec3_to_vec2(target.global_position)
+	point /= Targets.size()
+	
+	return point
 
 func _calculate_unprojected_rect() -> Rect2:
 	var rect := Rect2(unproject_position(Targets[0].global_position), Vector2())
@@ -42,53 +51,22 @@ func _calculate_unprojected_rect() -> Rect2:
 		rect = rect.expand(unproject_position(Targets[index].global_position))
 	return rect
 
-func _calculate_targets_rect() -> Rect2:
-	var rect := Rect2(_vec3_to_vec2(Targets[0].global_position), Vector2())
-	for index in Targets.size():
-		if index == 0:
-			continue
-		rect = rect.expand(_vec3_to_vec2(Targets[index].global_position))
-	return rect
-
-func _calc_center(rect: Rect2) -> Vector2:
-	return Vector2(rect.position.x + rect.size.x / 2, rect.position.y + rect.size.y / 2)
-
-func _calculate_position(rect: Rect2, unprojected_rect: Rect2) -> float:
-	var center := _calc_center(rect)
+func _calculate_position(unprojected_rect: Rect2) -> Vector3:
+	var center: Vector2 = _calculate_center()
+	var view_rect_ratio: float = unprojected_rect.size.x / _viewport_rect.size.x
 	if _initial_proportion == 0:
-		_initial_proportion = unprojected_rect.size.x / _viewport_rect.size.x
+		_initial_proportion = view_rect_ratio
 	if _initial_distance == 0:
-		_initial_distance = global_position.distance_to(Targets[0].global_position)
+		_initial_distance = global_position.distance_to(Vector3(center.x, global_position.y, 0))
 	
-	var z_pos := global_position.z
 	# FIXME: The jitter is caused by this. MoveSpeed is making the z go back and fourth.
-	if unprojected_rect.size.x / _viewport_rect.size.x > _initial_proportion:
-		z_pos = global_position.z + MoveSpeed
-	elif unprojected_rect.size.x / _viewport_rect.size.x < _initial_proportion:
-		z_pos = global_position.z - MoveSpeed
-	return clamp(z_pos, _initial_distance, MaxZoom)
-
-
-func _multi_target() -> void: 
-	if not Targets:
-		return
+	var move_speed: float = MaxMoveSpeed
 	
-	# Keep camera centered between the targets
-	var point: Vector2 = Vector2.ZERO
-	for target in Targets:
-		point += _vec3_to_vec2(target.position)
-	point /= Targets.size()
-	position = lerp(position, Vector3(point.x, position.y, position.z), MoveSpeed)
-	
-	# Find the zoom that will contain the targets
-	var rect: Rect2 = _calculate_targets_rect()
-	rect = rect.grow_individual(Margin.x, Margin.y, Margin.x, Margin.y)
-	var screen_size: Vector2 = get_viewport().get_visible_rect().size
-	var z: float = rect.size.x / screen_size.y * 2
-	fov = clamp(z, MinZoom, MaxZoom)
-	
-
-
+	if view_rect_ratio < _initial_proportion:
+		move_speed *= -1
+	var z_pos = lerp(global_position.z, global_position.z + move_speed, Smoothing)
+	Console.info("%s" % z_pos)
+	return Vector3(center.x, global_position.y, clamp(z_pos, _initial_distance, MaxZoom))
 
 
 
