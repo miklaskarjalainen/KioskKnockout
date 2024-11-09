a7fe938e65154161692032b009eedb6be9d39420
miklas karjalainen <miklas.karjalainen@gmail.com>
2024-11-09 23:19:27 +0200

    Add positions for hurtboxes to placed at.

--- src/actors/ActionController.gd
+++ src/actors/ActionController.gd
@@ -5,14 +5,19 @@ const BASIC_PUNCH = preload("res://src/actors/Attacks/BasicPunch.tscn")
 @onready var player: Player = get_parent()
 @export var anim_controller: AnimationController = null
 
+@onready var pos_rhand = Global.find_node_or_null(player, "pos_rhand")
+@onready var pos_lhand = Global.find_node_or_null(player, "pos_lhand")
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	assert(anim_controller)
 	assert(player, "is null")
 	assert(player is Player, "is not player")
+	assert(pos_rhand, "is null")
+	assert(pos_lhand, "is null")
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed(player.InputNormal):
 		anim_controller.play_action("punch")
-		# r_arm.add_child(BASIC_PUNCH.instantiate())
-	
+		pos_rhand.add_child(BASIC_PUNCH.instantiate())
--- src/singletons/Global.gd
+++ src/singletons/Global.gd
@@ -5,3 +5,18 @@ signal OptionsClosed()
 
 signal MasterVolumeChanged(new_value: float)
 signal MusicVolumeChanged(new_value: float)
+
+
+## Recursively finds node with the given name, from a parent node. 
+func find_node_or_null(parent: Node, target: String) -> Node:
+	for n in parent.get_children():
+		if n.name == target:
+			return n
+		
+		var found = find_node_or_null(n, target)
+		if found:
+			return found
+	
+	return null
+
+ 
