ac6275e3022c32f62b3d59fefc59c284b806635b
miklas karjalainen <miklas.karjalainen@gmail.com>
2024-11-11 15:35:00 +0200

    Added button buffers to map to actions.

--- src/actors/ActionController.gd
+++ src/actors/ActionController.gd
@@ -1,6 +1,7 @@
 extends Node
 class_name ActionController
 
+const ACTION_ERASE_FRAMES: int = 12
 const BASIC_PUNCH = preload("res://src/actors/Attacks/BasicPunch.tscn")
 
 ## Action name should be the same as the Animation name
@@ -12,6 +13,9 @@ signal action_ended()
 
 @onready var pos_rhand = Global.find_node_or_null(player, "pos_rhand")
 @onready var pos_lhand = Global.find_node_or_null(player, "pos_lhand")
+
+var _button_erase: int = -1
+var _button_buffer := []
 
 var _performing_action := false
 
@@ -27,20 +31,62 @@ func _ready() -> void:
 	
 	action_started.connect(func(_ac: String): _performing_action = true)
 	action_ended.connect(func(): _performing_action = false)
-	
 
-func _physics_process(delta: float) -> void:
-	if _performing_action:
+# Try to match action buffer to an action.
+func _submit_action():
+	if _button_buffer.size() == 0:
 		return
 	
-	if Input.is_action_just_pressed(player.InputNormal):
+	var buf = _button_buffer.duplicate()
+	_button_buffer.clear()
+	
+	# TODO: dashes
+	if buf.size() >= 2:
+		if buf[0] == "left" and buf[1] == "left":
+			Console.warning("Dashes not implemented")
+			return
+		elif buf[0] == "right" and buf[1] == "right":
+			Console.warning("Dashes not implemented")
+			return
+	
+	if buf[0] == "normal":
 		var atk: AttackHurtbox = BASIC_PUNCH.instantiate()
-		
-		Console.message("perform action: %s" % atk.play_anim)
 		action_started.emit(atk.play_anim)
 		atk.tree_exiting.connect(func():
 			action_ended.emit()
 		)
-		
 		atk.add_exclude(player)
 		pos_rhand.add_child(atk)
+
+# Adds a action to buffer
+func _push_action(str: String):
+	_button_buffer.push_front(str)
+	_button_erase = ACTION_ERASE_FRAMES
+	
+	while _button_buffer.size() > 3:
+		_button_buffer.pop_back()
+
+func _physics_process(delta: float) -> void:
+	Debug.add_line("btn_buffer", _button_buffer)
+	_handle_action_inputs()
+
+func _handle_action_inputs() -> void:
+	if not _performing_action:
+		
+		# TODO: Use opponent relative directions instead of "left" or "right".
+		if Input.is_action_just_pressed(player.InputLeft):
+			_push_action("left")
+		if Input.is_action_just_pressed(player.InputRight):
+			_push_action("right")
+		if Input.is_action_just_pressed(player.InputUp):
+			_push_action("up")
+		if Input.is_action_just_pressed(player.InputDown):
+			_push_action("down")
+		if Input.is_action_just_pressed(player.InputNormal):
+			_push_action("normal")
+			_submit_action()
+	
+	if _button_erase >= 0:
+		_button_erase -= 1
+		if _button_erase == -1:
+			_submit_action()
