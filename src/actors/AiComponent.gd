c62cd4154e2d71422fd9b0202886cae31c677555
miklas karjalainen <miklas.karjalainen@gmail.com>
2024-12-01 21:12:26 +0200

    Update AiComponent.gd

--- src/actors/AiComponent.gd
+++ src/actors/AiComponent.gd
@@ -26,28 +26,44 @@ func _weight_options() -> Dictionary:
 		opts[_last_action]   = 1000
 	
 	# If opponent is really far, prefer walking towards it.
-	if dist_to_opp() > 1.6:
-		opts[player.InputDown] = 0
-		opts[player.InputNormal] = 0
-		opts[player.InputSpecial] = 0
-		opts[player.InputUp] = 5
+	if dist_to_opp() > 1.85:
 		if player.is_opponent_on_right():
 			opts[player.InputRight] += 25
 		else:
 			opts[player.InputLeft] += 25
+		
 	elif not opponent.Health.is_dead():
 		# Otherwise prefer making an attack.
-		opts[player.InputDown] = 15
-		opts[player.InputNormal] = 25
-		opts[player.InputSpecial] = 25
+		opts[player.InputDown] = 10
+		opts[player.InputNormal] = 5
+		opts[player.InputSpecial] = 5
+		
+		# Further away, do a kick.
+		if dist_to_opp() > 1.65:
+			opts[player.InputSpecial] += 35
+		else:
+			opts[player.InputNormal] += 35
+		
+		# Try countering from air (Uppercut)
+		if not opponent.is_on_floor():
+			if player.Action._button_buffer.size() > 0 and not player.Action._button_buffer[0] == "up":
+				opts[player.InputUp] += 69
+			else:
+				opts[player.InputNormal] += 69
+	
+	if not player.is_on_floor():
+		opts[player.InputSpecial] += 90
 	
 	# Prefer using the same action as previous frame.
 	if not _last_action.is_empty():
-		opts[_last_action] += 100
+		opts[_last_action] += 150
 	
-	# Prefer blocking an attack
+	# Prefer blocking an attack, escpecially continueing if already began
 	if dist_to_opp() < 2.0 and opponent.Action.is_performing_action():
 		opts[player.InputDown] += 80
+		if _last_action == player.InputDown:
+			opts[player.InputDown] += 1000
+	
 	
 	return opts
 
