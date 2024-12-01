2af9130db289af8b309bc323ccd88f6fc379d08f
miklas karjalainen <miklas.karjalainen@gmail.com>
2024-12-01 17:05:04 +0200

    Button Hover sfx

---
+++ src/gui/SfxButton.gd
@@ -0,0 +1,15 @@
+extends Button
+class_name SfxButton
+
+@export var hover_sfx_path: String = "res://assets/sounds/sfx/sfx_hover.ogg"
+
+
+func _play_hover():
+	if disabled:
+		return
+	AudioManager.play(hover_sfx_path, AudioManager.Audio_Player.SFX)
+
+func _ready() -> void:
+	if hover_sfx_path != null:
+		mouse_entered.connect(_play_hover)
+		focus_entered.connect(_play_hover)
