e2902ad884b96fb1a6bde2ff4d23089a0f4e9e7a
miklas karjalainen <miklas.karjalainen@gmail.com>
2024-12-01 16:52:50 +0200

    Fix broken code @Zefyro

--- src/singletons/AudioManager.gd
+++ src/singletons/AudioManager.gd
@@ -12,19 +12,26 @@ var ui_volume: float
 var sfx_volume: float
 
 func _ready() -> void:
+	_sfx_player.play()
 	AudioManager.play("res://assets/sounds/music/Generating Worlds.ogg", AudioManager.Audio_Player.Music)
 
 func play(fpath: String, audio_channel: Audio_Player) -> AudioStreamPlayer:
 	var audio_player: AudioStreamPlayer
+	var stream: AudioStream = AudioStreamOggVorbis.load_from_file(fpath)
+	
 	match audio_channel:
 		Audio_Player.Music:
 			audio_player = _music_player
 		Audio_Player.UI:
 			audio_player = _ui_player
 		Audio_Player.SFX:
-			audio_player = _sfx_player
+			var playback := _sfx_player.get_stream_playback() as AudioStreamPlaybackPolyphonic
+			playback.play_stream(stream, 0, 0, randf_range(0.85, 1.15))
+			return _sfx_player
+		_:
+			Console.error("AudioManager: INVALID CHANNEL")
 	
-	audio_player.stream = AudioStreamOggVorbis.load_from_file(fpath)
+	audio_player.stream = stream
 	audio_player.play()
 	return audio_player
 
